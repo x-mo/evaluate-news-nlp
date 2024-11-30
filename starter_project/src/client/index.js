402ee1e236f580b2932c2710f232ca4e767ea1f9
Mohamed Awad <X_MohamedAhmed@yahoo.com>
2024-11-30 12:59:25 +0400

    Add js & sass imports

--- starter_project/src/client/index.js
+++ starter_project/src/client/index.js
@@ -1,5 +1,6 @@
 // js files
 import { handleSubmit } from './js/formHandler'
+import { checkForName } from './js/nameChecker'
 
 
 
@@ -7,3 +8,8 @@ import { handleSubmit } from './js/formHandler'
 // console.log("CHANGE!!");
 
 // sass files
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
